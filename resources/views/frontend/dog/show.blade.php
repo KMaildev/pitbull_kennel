c1b7c02eb5af93885aa910e362a357d44a7f27ba
ZinMinTun <kmailsoftwaredevelopment@gmail.com>
2022-12-26 15:26:06 +0630

    update

--- resources/views/frontend/dog/show.blade.php
+++ resources/views/frontend/dog/show.blade.php
@@ -1,11 +1,184 @@
 @extends('layouts.app')
-@section('title', ' - KENNELS')
+@section('title', ' - Dog')
 @section('content')
-
-    <section class="section-b-space shop-section" style="background-color: black">
+    <section class="product-section">
         <div class="container-fluid-lg">
             <div class="row">
+                <div class="col-xxl-12 col-xl-12 col-lg-12 wow fadeInUp">
+                    <div class="row g-4">
+                        <div class="col-xl-4 wow fadeInUp">
+                            <div class="product-left-box">
+                                <div class="row g-2">
+                                    <div class="col-xxl-10 col-lg-12 col-md-10 order-xxl-2 order-lg-1 order-md-2">
+                                        <div class="product-main-2 no-arrow">
+                                            @php
+                                                $dog = explode(',', $dog->photo);
+                                            @endphp
+                                            @foreach ($dog as $photo)
+                                                <div>
+                                                    <div class="slider-image">
+                                                        <img src="{{ $photo }}" id="img-1"
+                                                            data-zoom-image="{{ $photo }}"
+                                                            class="img-fluid image_zoom_cls-0 blur-up lazyload"
+                                                            alt="">
+                                                    </div>
+                                                </div>
+                                            @endforeach
+                                        </div>
+                                    </div>
 
+                                    <div class="col-xxl-2 col-lg-12 col-md-2 order-xxl-1 order-lg-2 order-md-1">
+                                        <div class="left-slider-image-2 left-slider no-arrow slick-top">
+                                            @foreach ($dog as $dog_image)
+                                                <div>
+                                                    <div class="sidebar-image">
+                                                        <img src="{{ $dog_image }}" class="img-fluid blur-up lazyload"
+                                                            alt="">
+                                                    </div>
+                                                </div>
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-xl-8 wow fadeInUp" data-wow-delay="0.1s">
+                            <div class="right-box-contain">
+                                <h2 class="name">Creamy Chocolate Cake</h2>
+                                <div class="price-rating">
+                                    <h3 class="theme-color price">$49.50 <del class="text-content">$58.46</del> <span
+                                            class="offer theme-color">(8% off)</span></h3>
+                                    <div class="product-rating custom-rate">
+                                        <ul class="rating">
+                                            <li>
+                                                <i data-feather="star" class="fill"></i>
+                                            </li>
+                                            <li>
+                                                <i data-feather="star" class="fill"></i>
+                                            </li>
+                                            <li>
+                                                <i data-feather="star" class="fill"></i>
+                                            </li>
+                                            <li>
+                                                <i data-feather="star" class="fill"></i>
+                                            </li>
+                                            <li>
+                                                <i data-feather="star"></i>
+                                            </li>
+                                        </ul>
+                                        <span class="review">23 Customer Review</span>
+                                    </div>
+                                </div>
+
+                                <div class="procuct-contain">
+                                    <p>Lollipop cake chocolate chocolate cake dessert jujubes. Shortbread sugar plum
+                                        dessert
+                                        powder cookie sweet brownie. Cake cookie apple pie dessert sugar plum muffin
+                                        cheesecake.
+                                    </p>
+                                </div>
+
+                                <div class="product-packege">
+                                    <div class="product-title">
+                                        <h4>Weight</h4>
+                                    </div>
+                                    <ul class="select-packege">
+                                        <li>
+                                            <a href="javascript:void(0)" class="active">1/2 KG</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0)">1 KG</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0)">1.5 KG</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0)">Red Roses</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0)">With Pink Roses</a>
+                                        </li>
+                                    </ul>
+                                </div>
+
+                                <div class="time deal-timer product-deal-timer mx-md-0 mx-auto" id="clockdiv-1"
+                                    data-hours="1" data-minutes="2" data-seconds="3">
+                                    <div class="product-title">
+                                        <h4>Hurry up! Sales Ends In</h4>
+                                    </div>
+                                    <ul>
+                                        <li>
+                                            <div class="counter d-block">
+                                                <div class="days d-block">
+                                                    <h5></h5>
+                                                </div>
+                                                <h6>Days</h6>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="counter d-block">
+                                                <div class="hours d-block">
+                                                    <h5></h5>
+                                                </div>
+                                                <h6>Hours</h6>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="counter d-block">
+                                                <div class="minutes d-block">
+                                                    <h5></h5>
+                                                </div>
+                                                <h6>Min</h6>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="counter d-block">
+                                                <div class="seconds d-block">
+                                                    <h5></h5>
+                                                </div>
+                                                <h6>Sec</h6>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+
+
+
+                                <div class="note-box product-packege">
+                                    <div class="cart_qty qty-box product-qty">
+                                        <div class="input-group">
+                                            <button type="button" class="qty-right-plus" data-type="plus" data-field="">
+                                                <i class="fa fa-plus" aria-hidden="true"></i>
+                                            </button>
+                                            <input class="form-control input-number qty-input" type="text"
+                                                name="quantity" value="0">
+                                            <button type="button" class="qty-left-minus" data-type="minus" data-field="">
+                                                <i class="fa fa-minus" aria-hidden="true"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+
+                                    <button onclick="location.href = 'cart.html';"
+                                        class="btn btn-md bg-dark cart-button text-white w-100">Add To Cart</button>
+                                </div>
+
+                                <div class="buy-box">
+                                    <a href="wishlist.html">
+                                        <i data-feather="heart"></i>
+                                        <span>Add To Wishlist</span>
+                                    </a>
+
+                                    <a href="compare.html">
+                                        <i data-feather="shuffle"></i>
+                                        <span>Add To Compare</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
             </div>
         </div>
     </section>
